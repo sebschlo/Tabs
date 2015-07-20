756f7b16313c7970dde5c9421ed6af9129ae988b
Sebastian Schloesser <s.schloesser@gmail.com>
2015-07-20 15:50:43 -0400

    Initial Commit

---
+++ TabsUITests/TabsUITests.swift
@@ -0,0 +1,34 @@
+//
+//  TabsUITests.swift
+//  TabsUITests
+//
+//  Created by Schloesser, Sebastian on 7/20/15.
+//  Copyright © 2015 Schloesser, Sebastian. All rights reserved.
+//
+
+import XCTest
+
+class TabsUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
